bff544b2a5adc0a9958b276753d3bb1b1ac0e998
Samuel Matias Garcia Saboya <samuel.alten@congreso.int>
2020-01-16 09:42:56 +0100

    Primer commit

---
+++ template-apirest/src/test/java/com/congreso/springboot/apirest/TemplateApirestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.congreso.springboot.apirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemplateApirestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
