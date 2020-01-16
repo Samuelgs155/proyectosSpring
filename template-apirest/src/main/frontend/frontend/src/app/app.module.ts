f4e151e818afd7e4ce3e497b9700508e83e23295
Samuel Matias Garcia Saboya <samuel.alten@congreso.int>
2020-01-16 12:37:19 +0100

    Commit con Angular incrustado en el Spring Boot

---
+++ template-apirest/src/main/frontend/frontend/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { EcommerceComponent } from './ecommerce/ecommerce.component';
+import { ProductsComponent } from './ecommerce/products/products.component';
+import { OrdersComponent } from './ecommerce/orders/orders.component';
+import { ShoppingCartComponent } from './ecommerce/shopping-cart/shopping-cart.component';
+import { EcommerceService } from './ecommerce/services/ecommerce-service.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EcommerceComponent,
+    ProductsComponent,
+    OrdersComponent,
+    ShoppingCartComponent
+  ],
+  imports: [
+    BrowserModule,
+        HttpClientModule,
+        FormsModule,
+        ReactiveFormsModule
+  ],
+  providers: [
+    EcommerceService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
